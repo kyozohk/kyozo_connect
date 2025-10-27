69e4082e6ef62a4b3bf3b4467f80bd30c67f3c9f
Kyozo Tech <tech@kyozo.com>
2025-10-27 23:56:14 +0000

    Try fixing this error: `Runtime Error: Error: The default Firebase app d

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -2,18 +2,22 @@
 
 import admin from 'firebase-admin';
 
+let app;
+
 if (!admin.apps.length) {
   try {
     const serviceAccount = JSON.parse(
       process.env.FIREBASE_SERVICE_ACCOUNT_KEY as string
     );
-    admin.initializeApp({
+    app = admin.initializeApp({
       credential: admin.credential.cert(serviceAccount),
     });
   } catch (error) {
     console.error('Firebase admin initialization error', error);
   }
+} else {
+  app = admin.app();
 }
 
-export const adminAuth = admin.auth();
-export const adminDb = admin.firestore();
+export const adminAuth = admin.auth(app);
+export const adminDb = admin.firestore(app);
