0c76824e88d83d052d6e3b22e621bf83061ada8a
Kyozo Tech <tech@kyozo.com>
2025-10-28 14:36:24 +0000

    error-boundary-callbacks.ts:101 Uncaught Error: Invalid src prop (https:

--- next.config.ts
+++ next.config.ts
@@ -39,6 +39,12 @@ const nextConfig: NextConfig = {
         hostname: 'api.dicebear.com',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.imgur.com',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
